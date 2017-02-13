acf2aac0f8ca25ea4749b88fe5dcb4fb91d994ac
Akinyele Thompson <akinyele.thompson@mymona.uwi.edu>
2017-02-13 01:19:36 +0000

    final commit

--- sendemail.py
+++ sendemail.py
@@ -13,8 +13,8 @@ Subject: {}
 
 message_to_send = message.format(from_name, from_addr, to_name,to_addr, subject, msg)
 # Credentials (if needed)
-username = 'akinyelethompson@gmail.com'
-password = 'dkca ejuj nufl qyiy'
+username = ''
+password = ''
 # The actual mail send
 server = smtplib.SMTP('smtp.gmail.com:587')
 server.starttls()
